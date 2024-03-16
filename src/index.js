1ec594ad90104b3e904c8a387bc90d2391694b95
Thorantt <86085299+Thorantt@users.noreply.github.com>
2024-03-15 20:32:28 -0400

    l

--- src/index.js
+++ src/index.js
@@ -2,59 +2,27 @@ const chatForm = get('form');
 const chatInput = get('input');
 const chatBox = get('main');
 
-appendMessage('bot', 'This is a bot bubble');
-appendMessage('user', 'This is a user bubble');
-appendMessage("bot", "Is your situation urgent");
-appendMessage("bot", "Please call 911 or go to the amergency");
-appendMessage("bot", 'What are your symptoms');
-appendMessage("bot", 'What is your assigned at birth gender and your age')
-//diagnostics
-//Ingestion
-  //Child
-  //Lack of breathing
-  //Poison
-  //Toxins
-  //Cleaning products
-  //Extreme
-//Chest pains
-  //Heart pains
-  //Stomach pains
-  //Lung pain
-  //Extreme
-//Head pains
-  //Persitent
-  //Short lasting
-  //Extreme
-//Muscles pains
-  //Upper body pains
-  //Lower body
-//Throwing up
-  //Chronic
-  //Painful
-  //Blood
-//Expulses
-  //Blood
-  //Puss
-  //Unknown matter
-  //Pain
-//Coordination problems
-  //Chronic
-  //Sudden
-//Fever
-  //Amount
-//Other
-appendMessage("bot","Do you take any medication?")
-appendMessage("bot", "We recommend you to call the 811 if your problem still isn't solved and it isn't an emergency. Otherwise, you should call 911 or go to the emergency.")
 
 
+start()
+
+
+//appendMessage("bot", 'What is your assigned at birth gender and your age')
+//appendMessage("bot","Do you take any medication?")
+//appendMessage("bot", "We recommend you to call the 811 if your problem still isn't solved and it isn't an emergency. Otherwise, you should call 911 or go to the emergency.")
+
+async function take_input(){
+
+}
 chatForm.addEventListener('submit', event => {
   event.preventDefault();
   const text = chatInput.value;
   if (!text) return;
-  
+  take_input()
   appendMessage('user', text);
   chatInput.value = '';
 });
+
 
 function appendMessage(side, text) {
   const bubble = `
@@ -68,10 +36,100 @@ function appendMessage(side, text) {
 function appendCheckbox(side, text){
   const boxes=`
     <div>
-    <input`
+    <label><input class="messageCheckbox" type="checkbox" id="symptom9" name="Ingestion" value="checked">Ingestion</label>
+    </div>
+    <div>
+    <label><input class="messageCheckbox" type="checkbox" id="symptom8" name="Chest pain" value="checked">Chest pain</label>
+    </div>
+    <div>
+    <label><input class="messageCheckbox" type="checkbox" id="symptom7" name="Head pain" value="checked">Head pain</label>
+    </div>
+    <div>
+    <label><input class="messageCheckbox" type="checkbox" id="symptom6" name="Muscle pain" value="checked">Muscle pain</label>
+    </div>
+    <div>
+    <label><input class="messageCheckbox" type="checkbox" id="symptom5" name="Throwing up" value="checked">Throwing up</label>
+    </div>
+    <div>
+    <label><input class="messageCheckbox" type="checkbox" id="symptom4" name="Pee or excrement" value="checked">Pee or excrements</label>
+    </div>
+    <div>
+    <label><input class="messageCheckbox" type="checkbox" id="symptom3" name="Coordination problem" value="checked">Coordination problems</label>
+    </div>
+    <div>
+    <label><input class="messageCheckbox" type="checkbox" id="symptom2" name="Fever" value="checked">Fever</label>
+    </div>
+    <div>
+    <label><input class="messageCheckbox" type="checkbox" id="symptom1" name="Other" value="checked">Other</label>
+    </div>
+    <button id="submitCheckboxesb">"Submit"</button>`
+  
+    
+    chatBox.insertAdjacentHTML('beforeend', boxes);
+  chatBox.scrollTop += 500;
+  const submitCheckboxes=document.querySelector('#submitCheckboxesb')
+  submitCheckboxes.addEventListener('click', (event)=>{
+    let checkboxes = document.querySelectorAll('.messageCheckbox:checked');
+    let values = [];
+      checkboxes.forEach((checkbox) => {
+        values.push(checkbox.name);
+      });
+      nextt();
+  })
 }
 
 // Utils
 function get(selector, root = document) {
   return root.querySelector(selector);
+}
+async function start(){
+  appendMessage("bot", "Is your situation urgent?");
+  const boxe=`<div><input class='approve' type='checkbox' name='yes'>Yes</div>
+  <div><input class='approve' type='checkbox' name='no'>No</div>
+  <button id="submitCheckboxesa">"Submit"</button>'`
+  chatBox.insertAdjacentHTML("beforeend",boxe)
+  const submitCheckboxes=document.querySelector('#submitCheckboxesa')
+  await submitCheckboxes.addEventListener('click', (event)=>{
+    let checkboxes = document.querySelectorAll('.approve:checked');
+    let values = [];
+      checkboxes.forEach((checkbox) => {
+        values.push(checkbox.name);
+      });
+      console.log(values[0])
+    if (values[0]=='yes'){
+      appendMessage("bot", "Please call 911 or go to the emergency");
+    }
+    else {
+      appendMessage("bot", "You don't have to go to the hospital. Please help me answer some questions about you, to better help solve your problem. What are your symptoms");
+      appendCheckbox();
+    }
+  })
+  
+};
+function nextt(){
+  appendMessage("bot", 'What is your assigned at birth gender')
+  const boxe=`<div><input class='gender' type='checkbox' name='male'>Male</div>
+  <div><input class='gender' type='checkbox' name='female'>Female</div>
+  <button id="submitCheckboxesc">"Submit"</button>'`
+  chatBox.insertAdjacentHTML("beforeend",boxe)
+  const submitCheckboxes=document.querySelector('#submitCheckboxesc')
+    submitCheckboxes.addEventListener('click', (event)=>{
+    let checkboxes = document.querySelectorAll('.gender:checked');
+    let values = [];
+      checkboxes.forEach((checkbox) => {
+        values.push(checkbox.name);
+      });
+      nexttt()
+  })
+};
+function nexttt(){
+  appendMessage("bot", 'What is your age')
+  chatForm.addEventListener('submit', event => {
+    event.preventDefault();
+    const text = chatInput.value;
+    if (!text) return;
+    take_input()
+    appendMessage('user', text);
+    chatInput.value = '';
+  });
 }
